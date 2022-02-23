fd816cbc36f9ac264f0c4f3d20ba6b381081b3f7
hychen <hychen@uic.edu>
2022-02-23 17:41:10 -0600

    Minor update - 2-23-22

--- R/dat.R
+++ R/dat.R
@@ -1,8 +1,8 @@
-#' A 400 * 9 matrix to test the function within the package
+#' A 400x9 matrix to test the function within the package
 #'
-#' A 400 * 9 matrix to test the function within the package.
+#' A 400x9 matrix to test the function within the package.
 #'
-#' @format A datam matrix whose dimension is 400x9.
+#' @format A data matrix whose dimension is 400x9.
 #'
 #'
 "dat"
