79a8202dd624c6e5d8e9faba95738dd1ff7dfe79
Hua Yun Chen <hychen@uic.edu>
2023-08-15 17:52:35 -0500

    update

--- R/dat.R
+++
@@ -1,8 +0,0 @@
-#' A 400x9 matrix to test the function within the package
-#'
-#' A 400x9 matrix to test the function within the package.
-#'
-#' @format A data matrix whose dimension is 400x9.
-#'
-#'
-"dat"
---
+++ R/example.R
@@ -0,0 +1,9 @@
+#'
+#' Example data set
+#'
+#' Example data is a 400x9 matrix to test the function within the package.
+#'
+#' @format A data matrix whose dimension is 400x9.
+#'
+#'@export
+"example"
