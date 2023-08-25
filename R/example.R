e24c5104e7a8ceeda04ea362e4b902bd38164b0b
Hua Yun Chen <hychen@uic.edu>
2023-08-25 16:17:44 -0500

    update

--- R/example.R
+++ R/example.R
@@ -3,7 +3,11 @@
 #'
 #' Example data is a 400x9 matrix to test the function within the package.
 #'
-#' @format A data matrix whose dimension is 400x9.
+#' @format A data matrix with 400 records and 9 variables.
 #'
-#'@export
+#' @source simulated data
+#'
 "example"
+
+
+
