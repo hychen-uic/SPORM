d329692af38e295b1c7835577c9814e9f564282b
Hua Yun Chen <hychen@uic.edu>
2024-03-05 10:22:32 -0600

    update

---
+++ R/bone.R
@@ -0,0 +1,12 @@
+#'
+#' Bone fracture data set
+#'
+#' This data set contains data from the case-control study of hip fracture
+#' in veterans.
+#'
+#' @format A data matrix with 436 records and 28 variables. -9 in any variable
+#'         denotes missing value. race and age are matching variables.
+#'
+#' @source A case-control study of hip fractures in veteran,
+#'
+"bone"
--- R/example.R
+++ R/example.R
@@ -1,12 +1,14 @@
 #'
-#' Example data set
+#' Simulated data set
 #'
-#' Example data is a 400x9 matrix to test the function within the package.
+#' Example simulated data is a 400x9 matrix to test the function
+#' within the package.
 #'
 #' @format A data matrix with 400 records and 9 variables.
 #'
 #' @source simulated data
 #'
+
 "example"
 
 
